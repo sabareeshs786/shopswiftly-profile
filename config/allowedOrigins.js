fd218bdd590445e222aa20f7f9832260326b2db1
Hackovator <ssabareesh646@gmail.com>
2024-01-20 22:49:15 +0530

    Update allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:3001'
 ];
 
 module.exports = allowedOrigins;
